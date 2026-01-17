569bbfdca25b48b32c4f165aa788860e73dadf70
Gbeke18 <gbekeiyun@gmail.com>
2026-01-17 10:11:16 +0100

    Update app.py

--- Wine Cultivar origin/app.py
+++ Wine Cultivar origin/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "wine_cultivar_model.pkl")
+
+model, scaler, features = joblib.load(MODEL_PATH)
 
 app = Flask(__name__)
-
-# Load model
-model, scaler, features = joblib.load("wine_cultivar_model.pkl")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -22,4 +25,5 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
